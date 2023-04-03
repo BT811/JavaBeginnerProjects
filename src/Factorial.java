9acee392d41dcbba9b0ece8864958f944e3db1e2
BT811 <99087700+BT811@users.noreply.github.com>
2023-04-03 11:38:12 +0300

    Add files via upload

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
-public class FindMultiplesOfTwo {
+
+public class Factorial {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num,total = 0;
+        int num, total = 1;
 
         System.out.println("Enter the number...");
         num = input.nextInt();
 
-        for (int i = 1 ; i <= num ; i*=2) System.out.println(i);
+        for (int i = 1 ; i <= num ; i++) total *= i ;
+
+        System.out.println(total);
+
 
     }
 }
